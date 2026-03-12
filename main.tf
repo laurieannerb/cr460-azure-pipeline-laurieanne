ed3c87835b10d48622fd2067ddc10308b701973b
Laurie-Anne Rivard <ton_email_github@example.com>
2026-03-11 23:01:39 -0400

    rebuild terraform config for vm deployment

--- main.tf
+++ main.tf
@@ -6,18 +6,21 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-cr460-laurieanne"
   location = "East US"
 }
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-cr460-laurieanne"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-cr460-laurieanne"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
 resource "azurerm_network_interface" "nic" {
   name                = "nic-cr460-laurieanne"
   location            = azurerm_resource_group.rg.location
@@ -29,11 +32,12 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-cr460-laurieanne"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size = "Standard_B1ms"
+  size                = "Standard_B2s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
