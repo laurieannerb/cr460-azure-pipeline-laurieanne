201c80b02fa25a7f61249e778f2d27d4a880d0fa
Laurie-Anne Rivard <ton_email_github@example.com>
2026-03-11 22:13:43 -0400

    add virtual network

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-cr460-laurieanne"
   location = "Canada Central"
 }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-cr460-laurieanne"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
