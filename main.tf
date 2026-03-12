74006347f4aeec03eadb0dd649601fd417e72220
Laurie-Anne Rivard <ton_email_github@example.com>
2026-03-11 23:37:07 -0400

    add docker container

--- main.tf
+++ main.tf
@@ -58,4 +58,28 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}resource "azurerm_container_group" "container" {
+  name                = "aci-cr460-laurieanne"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-laurieanne-cr460"
+  os_type             = "Linux"
+
+  container {
+    name   = "nginx"
+    image  = "nginx:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  exposed_port {
+    port     = 80
+    protocol = "TCP"
+  }
 }
